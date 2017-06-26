ef6726ecf012d566b9fa7a197e874fd474439838
Roman Doroshenko <roman.doroshenko.tlt@gmail.com>
2017-06-26 16:26:52 +0400

    initiated project with npm

---
+++ initData.js
@@ -0,0 +1,12 @@
+[
+  {
+    articleId: '987654',
+    articleTitle: 'Article one',
+    articleContent: 'Here goes the content of the article'
+  },
+  {
+    articleId: '123456',
+    articleTitle: 'Article two',
+    articleContent: 'Another portion of the content'
+  }
+]
